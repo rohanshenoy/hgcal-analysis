81b27a43978b082bd60eb66eaae84a725605498c
Danny Noonan <dnoonan@cern.ch>
2021-10-04 10:35:46 -0500

    move into function that could be called in other code

--- TrainingData/produceTrainingData.py
+++ TrainingData/produceTrainingData.py
@@ -3,58 +3,73 @@ import numpy as np
 import pandas as pd
 import awkward as ak
 
-import argparse
+def loadTrainingData(inputRootFile,
+                     rootFileTDirectory='FloatingpointThreshold0DummyHistomaxGenmatchGenclustersntuple',
+                     outputFileName='CALQ.csv',
+                     N_eLinks=5,
+                     useADC=False,
+                     ):
+    _tree = uproot.open(inputRootFile)[f'{rootFileTDirectory}/HGCalTriggerNtuple']
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i','--input',dest='inputRootFile', default='ntuple.root', help="TPG Ntuple file to process")
-parser.add_argument('-d','--dir',dest='rootFileTDirectory', default='FloatingpointThreshold0DummyHistomaxGenmatchGenclustersntuple', help="Directory within input root file to find HGCalTriggerNtuple TTree")
-parser.add_argument('-N',dest='N_eLinks',type=int,default=5,help='Number of eRx to select')
-parser.add_argument('--ADC',dest='useADC',default=False,action='store_true',help='Use ADC rather than transverse ADC')
-parser.add_argument('-o','--output',dest='outputFileName',default='CALQ.csv',help='Output file name (either a .csv or .pkl file name)')
+    hasSimEnergy = 'tc_simenergy' in _tree
+    if hasSimEnergy:
+        arrays = _tree.arrays(['tc_zside','tc_layer','tc_waferu','tc_waferv','tc_cellu','tc_cellv','tc_data','tc_eta','tc_simenergy'])
+    else:
+        arrays = _tree.arrays(['tc_zside','tc_layer','tc_waferu','tc_waferv','tc_cellu','tc_cellv','tc_data','tc_eta'])
 
-args = parser.parse_args()
+    select_eLinks = {5 : (arrays[b'tc_layer']==9),
+                     4 : (arrays[b'tc_layer']==7) | (arrays[b'tc_layer']==11),
+                     3 : (arrays[b'tc_layer']==13),
+                     2 : (arrays[b'tc_layer']<7) | (arrays[b'tc_layer']>13),
+                     -1 : (arrays[b'tc_layer']>0)}
 
+    assert N_eLinks in select_eLinks
 
-_tree = uproot.open(args.inputRootFile)[f'{args.rootFileTDirectory}/HGCalTriggerNtuple']
+    df = ak.to_pandas(arrays[select_eLinks[N_eLinks]])
 
-hasSimEnergy = 'tc_simenergy' in _tree
-if hasSimEnergy:
-    arrays = _tree.arrays(['tc_zside','tc_layer','tc_waferu','tc_waferv','tc_cellu','tc_cellv','tc_data','tc_eta','tc_simenergy'])
-else:
-    arrays = _tree.arrays(['tc_zside','tc_layer','tc_waferu','tc_waferv','tc_cellu','tc_cellv','tc_data','tc_eta'])
+    dfRemap = pd.read_csv('tcRemap.csv')
+    df = df.reset_index().merge(dfRemap)
+    df['ADCT'] = (df.tc_data* ((1./np.cosh(df.tc_eta))*2**12).astype(int)/2**12).astype(int)
 
-select_eLinks = {5 : (arrays[b'tc_layer']==9),
-                 4 : (arrays[b'tc_layer']==7) | (arrays[b'tc_layer']==11),
-                 3 : (arrays[b'tc_layer']==13),
-                 2 : (arrays[b'tc_layer']<7) | (arrays[b'tc_layer']>13),
-                 -1 : (arrays[b'tc_layer']>0)}
+    #create new unique index (can't get pivot working on multi-indexing, but this is essentially the same)
+    df['WaferEntryIdx'] = (df.entry*1000000 + df.tc_layer*10000 + df.tc_waferu*100 + df.tc_waferv)*df.tc_zside
 
-assert args.N_eLinks in select_eLinks
+    val = 'ADCT'
+    if useADC:
+        val='tc_data'
+    dfTrainData = df.pivot(index='WaferEntryIdx',columns='tc_cell_train',values=val).fillna(0).astype(int)
+    dfTrainData.columns = [f'CALQ_{i}' for i in range(48)]
 
-df = ak.to_pandas(arrays[select_eLinks[args.N_eLinks]])
+    dfTrainData[['entry','zside','layer','waferu','waferv']] = df.groupby(['WaferEntryIdx'])[['entry','tc_zside','tc_layer','tc_waferu','tc_waferv']].mean()
 
-dfRemap = pd.read_csv('tcRemap.csv')
-df = df.reset_index().merge(dfRemap)
-df['ADCT'] = (df.tc_data* ((1./np.cosh(df.tc_eta))*2**12).astype(int)/2**12).astype(int)
+    if hasSimEnergy:
+        dfTrainData['simenergy'] = df.groupby(['WaferEntryIdx'])[['tc_simenergy']].sum()
 
-#create new unique index (can't get pivot working on multi-indexing, but this is essentially the same)
-df['WaferEntryIdx'] = (df.entry*1000000 + df.tc_layer*10000 + df.tc_waferu*100 + df.tc_waferv)*df.tc_zside
+    dfTrainData.reset_index(drop=True,inplace=True)
 
-val = 'ADCT'
-if args.useADC:
-    val='tc_data'
-dfTrainData = df.pivot(index='WaferEntryIdx',columns='tc_cell_train',values=val).fillna(0).astype(int)
-dfTrainData.columns = [f'CALQ_{i}' for i in range(48)]
+    if '.csv' in outputFileName:
+        dfTrainData.to_csv(outputFileName,index=False)
+    if '.pkl' in outputFileName:
+        dfTrainData.to_pickle(outputFileName)
+    if '.h5' in outputFileName:
+        dfTrainData.to_hdf(outputFileName, key='df', mode='w')
 
-dfTrainData[['entry','zside','layer','waferu','waferv']] = df.groupby(['WaferEntryIdx'])[['entry','tc_zside','tc_layer','tc_waferu','tc_waferv']].mean()
+    return dfTrainData
 
-if hasSimEnergy:
-    dfTrainData['simenergy'] = df.groupby(['WaferEntryIdx'])[['tc_simenergy']].sum()
+if __name__=="__main__":
+    import argparse
 
-dfTrainData.reset_index(drop=True,inplace=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i','--input',dest='inputRootFile', default='ntuple.root', help="TPG Ntuple file to process")
+    parser.add_argument('-d','--dir',dest='rootFileTDirectory', default='FloatingpointThreshold0DummyHistomaxGenmatchGenclustersntuple', help="Directory within input root file to find HGCalTriggerNtuple TTree")
+    parser.add_argument('-N',dest='N_eLinks',type=int,default=5,help='Number of eRx to select')
+    parser.add_argument('--ADC',dest='useADC',default=False,action='store_true',help='Use ADC rather than transverse ADC')
+    parser.add_argument('-o','--output',dest='outputFileName',default='CALQ.csv',help='Output file name (either a .csv or .pkl file name)')
 
-if '.csv' in args.outputFileName:
-    dfTrainData.to_csv(args.outputFileName,index=False)
-if '.pkl' in args.outputFileName:
-    import pickle
-    pickle.dump(dfTrainData, open(args.outputFileName,'wb'))
+    args = parser.parse_args()
+
+    df = loadTrainingData(inputRootFile = args.inputRootFile,
+                          rootFileTDirectory = args.rootFileTDirectory,
+                          outputFileName = args.outputFileName,
+                          N_eLinks = args.N_eLinks,
+                          useADC = args.useADC)
