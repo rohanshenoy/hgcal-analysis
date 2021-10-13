d08070f58107a42b9a6af241fc239748243c441d
rohanshenoy <42732515+rohanshenoy@users.noreply.github.com>
2021-10-13 15:25:07 -0700

    Update train_emdloss.py

--- train_emdloss.py
+++ train_emdloss.py
@@ -46,7 +46,7 @@ def main(args):
     if(not args.aeEMD):
         data=load_data(args)
     
-    current_directory='/ecoderemdvol'
+    current_directory=os.getcwd()
 
     #Data to track the performance of various EMD_CNN models
 
