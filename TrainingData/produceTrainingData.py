8c6a730cb92af7680876b5a0192b5d3679ded75c
rohanshenoy <42732515+rohanshenoy@users.noreply.github.com>
2022-02-10 16:56:00 -0800

    Larger datasets, physics columns
    Read from multiple root files, with eta and phi mappings as new columns

--- TrainingData/produceTrainingData.py
+++ TrainingData/produceTrainingData.py
@@ -1,74 +1,93 @@
+import os
 import uproot
 import numpy as np
 import pandas as pd
 import awkward as ak
 
-def loadTrainingData(inputRootFile,
-                     rootFileTDirectory='FloatingpointThreshold0DummyHistomaxGenmatchGenclustersntuple',
+def loadTrainingData(inputRoot,
+                     rootFileTDirectory='hgcalTriggerNtuplizer',
                      outputFileName='CALQ.csv',
                      N_eLinks=5,
                      useADC=False,
                      ):
-    _tree = uproot.open(inputRootFile)[f'{rootFileTDirectory}/HGCalTriggerNtuple']
+    
+    mergeTrainingData = pd.DataFrame()
+    if os.path.isdir(inputRoot):
+        
+        for infile in os.listdir(inputRoot):
+            if os.path.isdir(inputRoot+infile): continue
+            inputRootFile = os.path.join(inputRoot,infile)
+            
+            _tree = uproot.open(inputRootFile)[f'{rootFileTDirectory}/HGCalTriggerNtuple']
 
-    hasSimEnergy = 'tc_simenergy' in _tree
-    if hasSimEnergy:
-        arrays = _tree.arrays(['tc_zside','tc_layer','tc_waferu','tc_waferv','tc_cellu','tc_cellv','tc_data','tc_eta','tc_simenergy'])
-    else:
-        arrays = _tree.arrays(['tc_zside','tc_layer','tc_waferu','tc_waferv','tc_cellu','tc_cellv','tc_data','tc_eta'])
+            hasSimEnergy = 'tc_simenergy' in _tree
+            if hasSimEnergy:
+                arrays = _tree.arrays(['tc_zside','tc_layer','tc_waferu','tc_waferv','tc_cellu','tc_cellv','tc_data','tc_eta','tc_simenergy'])
+            else:
+                arrays = _tree.arrays(['tc_zside','tc_layer','tc_waferu','tc_waferv','tc_cellu','tc_cellv','tc_data','tc_eta'])
 
-    select_eLinks = {5 : (arrays[b'tc_layer']==9),
-                     4 : (arrays[b'tc_layer']==7) | (arrays[b'tc_layer']==11),
-                     3 : (arrays[b'tc_layer']==13),
-                     2 : (arrays[b'tc_layer']<7) | (arrays[b'tc_layer']>13),
-                     -1 : (arrays[b'tc_layer']>0)}
+            select_eLinks = {5 : (arrays[b'tc_layer']==9),
+                             4 : (arrays[b'tc_layer']==7) | (arrays[b'tc_layer']==11),
+                             3 : (arrays[b'tc_layer']==13),
+                             2 : (arrays[b'tc_layer']<7) | (arrays[b'tc_layer']>13),
+                             -1 : (arrays[b'tc_layer']>0)}
 
-    assert N_eLinks in select_eLinks
+            assert N_eLinks in select_eLinks
 
-    df = ak.to_pandas(arrays[select_eLinks[N_eLinks]])
+            df = ak.to_pandas(arrays[select_eLinks[N_eLinks]])
 
-    dfRemap = pd.read_csv('tcRemap.csv')
-    df = df.reset_index().merge(dfRemap)
-    df['ADCT'] = (df.tc_data* ((1./np.cosh(df.tc_eta))*2**12).astype(int)/2**12).astype(int)
+            dfRemap = pd.read_csv('tcRemap.csv')
+            df = df.reset_index().merge(dfRemap)
 
-    #create new unique index (can't get pivot working on multi-indexing, but this is essentially the same)
-    df['WaferEntryIdx'] = (df.entry*1000000 + df.tc_layer*10000 + df.tc_waferu*100 + df.tc_waferv)*df.tc_zside
+            df['ADCT'] = (df.tc_data* ((1./np.cosh(df.tc_eta))*2**12).astype(int)/2**12).astype(int)
 
-    val = 'ADCT'
-    if useADC:
-        val='tc_data'
-    dfTrainData = df.pivot(index='WaferEntryIdx',columns='tc_cell_train',values=val).fillna(0).astype(int)
-    dfTrainData.columns = [f'CALQ_{i}' for i in range(48)]
+            #create new unique index (can't get pivot working on multi-indexing, but this is essentially the same)
+            df['WaferEntryIdx'] = (df.entry*1000000 + df.tc_layer*10000 + df.tc_waferu*100 + df.tc_waferv)*df.tc_zside
 
-    dfTrainData[['entry','zside','layer','waferu','waferv']] = df.groupby(['WaferEntryIdx'])[['entry','tc_zside','tc_layer','tc_waferu','tc_waferv']].mean()
+            val = 'ADCT'
+            if useADC:
+                val='tc_data'
+            dfTrainData = df.pivot(index='WaferEntryIdx',columns='tc_cell_train',values=val).fillna(0).astype(int)
+            dfTrainData.columns = [f'CALQ_{i}' for i in range(48)]
 
-    if hasSimEnergy:
-        dfTrainData['simenergy'] = df.groupby(['WaferEntryIdx'])[['tc_simenergy']].sum()
+            dfTrainData[['entry','zside','layer','waferu','waferv']] = df.groupby(['WaferEntryIdx'])[['entry','tc_zside','tc_layer','tc_waferu','tc_waferv']].mean()
 
-    dfTrainData.reset_index(drop=True,inplace=True)
+            if hasSimEnergy:
+                dfTrainData['simenergy'] = df.groupby(['WaferEntryIdx'])[['tc_simenergy']].sum()
+
+            #Mapping wafer_u,v to physical coordinates
+            dfEtaPhi=pd.read_csv('WaferEtaPhiMap.csv')
+            dfTrainData=dfTrainData.merge(dfEtaPhi, on=['layer','waferu','waferv'])
+            dfTrainData.reset_index(drop=True,inplace=True)
+            mergeTrainingData=pd.concat([mergeTrainingData,dfTrainData])
+            
+            
+
+            
+    
 
     if '.csv' in outputFileName:
-        dfTrainData.to_csv(outputFileName,index=False)
+        mergeTrainingData.to_csv(outputFileName,index=False)
     if '.pkl' in outputFileName:
-        dfTrainData.to_pickle(outputFileName)
+        mergeTrainingData.to_pickle(outputFileName)
     if '.h5' in outputFileName:
-        dfTrainData.to_hdf(outputFileName, key='df', mode='w')
+        mergeTrainingData.to_hdf(outputFileName, key='df', mode='w')
 
-    return dfTrainData
+    return mergeTrainingData
 
 if __name__=="__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i','--input',dest='inputRootFile', default='ntuple.root', help="TPG Ntuple file to process")
-    parser.add_argument('-d','--dir',dest='rootFileTDirectory', default='FloatingpointThreshold0DummyHistomaxGenmatchGenclustersntuple', help="Directory within input root file to find HGCalTriggerNtuple TTree")
+    parser.add_argument('-i','--input',dest='inputRoot', default='ntuple.root', help="TPG Ntuple directory/file to process")
+    parser.add_argument('-d','--dir',dest='rootFileTDirectory', default='hgcalTriggerNtuplizer', help="Directory within input root file to find HGCalTriggerNtuple TTree")
     parser.add_argument('-N',dest='N_eLinks',type=int,default=5,help='Number of eRx to select')
     parser.add_argument('--ADC',dest='useADC',default=False,action='store_true',help='Use ADC rather than transverse ADC')
     parser.add_argument('-o','--output',dest='outputFileName',default='CALQ.csv',help='Output file name (either a .csv or .pkl file name)')
 
     args = parser.parse_args()
 
-    df = loadTrainingData(inputRootFile = args.inputRootFile,
+    df = loadTrainingData(inputRoot = args.inputRoot,
                           rootFileTDirectory = args.rootFileTDirectory,
                           outputFileName = args.outputFileName,
                           N_eLinks = args.N_eLinks,
