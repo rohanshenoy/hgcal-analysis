78222b47919128153b597ffdae37c396c58b9da6
rohanshenoy <42732515+rohanshenoy@users.noreply.github.com>
2022-02-17 23:55:23 -0800

    Update emdloss.py

--- emdloss.py
+++ emdloss.py
@@ -37,7 +37,7 @@ arrange443 = np.array([0,16, 32,
 
 def get_emd_loss(model_number):
     
-    model_path=os.getcwd()+'/best_emd/'+str(model_number)+'.h5'
+    model_path=os.getcwd()+'/22_best_emd/'+str(model_number)+'.h5'
     emd_model = tf.keras.models.load_model(model_path)
     emd_model.trainable = False
                  
